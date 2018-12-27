thought last commit was the big milestone, but there was a bug that I just fixed.  The bug had to do with rotation; the masks weren't oriented properly right outta the segmentation map  b/c x is 'up' when in 2d numpy but we didn't want that when working with 3-D numpy.   also in viz.py added generalized version of voxels.py from the plt website so we can quickly visualize small voxels.

blender_render_____from_npy.py
@@ -63,10 +63,12 @@ def make_faces(faces_arr):
   return tups
 #================================================================
 
-verts_filename='/home/u/p/fresh____as_of_Dec_12_2018/vr_mall____fresh___Dec_12_2018/verts.npy'
+# TODO: edit 'filename' variables to have current verts and faces.npy files
+verts_filename='/home/u/p/fresh____as_of_Dec_12_2018/vr_mall____fresh___Dec_12_2018/verts_nathan_.npy'
+faces_filename='/home/u/p/fresh____as_of_Dec_12_2018/vr_mall____fresh___Dec_12_2018/faces_nathan_.npy'
+
 verts_arr = np.load(verts_filename)
 verts = make_verts(verts_arr)
-faces_filename='/home/u/p/fresh____as_of_Dec_12_2018/vr_mall____fresh___Dec_12_2018/faces.npy'
 faces_arr = np.load(faces_filename)
 faces = make_faces(faces_arr)
 
@@ -97,3 +99,4 @@ obj = bpy.data.objects.new("My_Object", mesh_data)
 scene = bpy.context.scene
 scene.objects.link(obj)
 obj.select = True
+

m_cubes.py
@@ -6,13 +6,33 @@ from skimage.draw import ellipsoid
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 
-body_model=np.load('body_nathan_.npy')
 #============================================================
-verts, faces, normals, values = measure.marching_cubes_lewiner(body_model, 0)
+def mesh_from_pt_cloud(model_np_arr):
+  '''
+    Marching Cubes algo; rudimentary point cloud ---> mesh algorithm
+    The format we've been using works just fine as input;
+      3D np arr (ie. arr.shape==(513,513,513)) where each arr[i,j,k] is either 0 (no body) or 1 (body)
+
+    Timing: model of shape (513,513,513)
+      real  0m4.610s
+      user  0m3.060s
+      sys 0m0.369s
+
+      wow, fast!
+  '''
+  verts, faces, normals, values = measure.marching_cubes_lewiner(model_np_arr, 0)
+  return verts, faces
+#============================================================
+def save_mesh(pt_cloud, faces_filename, verts_filename):
+  verts, faces, normals, values = measure.marching_cubes_lewiner(body_model, 0)
+  np.save('faces_nathan_.npy', faces)
+  np.save('verts_nathan_.npy', verts)
 #============================================================
 
-np.save('faces_nathan_.npy', faces)
-np.save('verts_nathan_.npy', verts)
+if __name__=='__main__':
+  body_model=np.load('body_nathan_.npy')
+  #============================================================
+  save_mesh(body_model, 'faces_nathan_.npy', 'verts_nathan_.npy')
 
 
 

model.py
@@ -1,14 +1,28 @@
 import  numpy as np
 from    copy import deepcopy
 import imageio as ii
-import scipy; from scipy import ndimage, misc
+import scipy; from scipy import ndimage, misc, rot90
 
-from viz import *
-from d import debug 
-from save import save
-from utils import pad_all
-from on_locs import rot8
-from hollow import hollow
+# our files/modules.  
+from viz      import *
+from d        import debug 
+from save     import save
+from utils    import pad_all
+from rot8     import rot8
+from hollow   import hollow
+from m_cubes  import mesh_from_pt_cloud, save_mesh
+# TODO: consolidate all of 'em into utils.py?  or a few, maybe called:
+#
+#   mesh.py
+#   mask.py
+#   seg.py
+#
+# alt:
+#   utils.py
+#
+#
+#
+
 
 #   NOTE:  z is "up" ("up" as in, think about a human being "upright"; the head is "up")
 
@@ -64,9 +78,9 @@ def test_human():
   #model     = np.ones(shape).astype('bool')
   model     = mask(model, mask_2d)
   print "before rot8();   \n\n"
-  show_cross_sections(model, axis='y', freq=250) # NOTE: good.  it worked this time
+  show_cross_sections(model, axis='y', freq=250)
 
-  angle     = 90.1  # 90.0  # 72.9
+  angle     = 90.0  # 90.0  # 72.9
   model     = rot8(model, angle)
   print "right after rot8();   \n\n"
   if debug:
@@ -77,15 +91,21 @@ def test_human():
   mask_2d = pad_all(seg.segment(mask_side___URL), shape_2d)
   model     = mask(model, mask_2d)
   print "after 2nd masking:    \n\n"
-  skin=hollow(model)
+  SKIN=False
+  if SKIN:
+    skin=hollow(model)
   if save:
     np.save('body_nathan_.npy', model)
-    np.save('skin_nathan_.npy', skin )
-  if debug:
-    show_all_cross_sections(model, freq=1)
-  show_all_cross_sections(model, freq=5)
-  show_all_cross_sections(skin , freq=5)
+    if SKIN:
+      np.save('skin_nathan_.npy', skin )
+    save_mesh(model)
+  else: # not save:
+    verts,faces=mesh_from_pt_cloud(model)
 
+  if debug:
+    show_all_cross_sections(model, freq=5)
+    if SKIN:
+      show_all_cross_sections(skin , freq=5)
   return model
 #====================================   end func def of test_human():   =======================================================
 

rot8.py
@@ -7,20 +7,24 @@ from d import debug
 
 
 #================================================================
-def on_locs_nonzero(a):
-  num_dims=3
-  on_voxels=np.nonzero(a) # as of Dec. 21, 2018 with the following versions, dtype is default 'int64'
-  #  numpy info from conda:
-  #python      2.7.15  h33da82c_4    conda-forge
-  #numpy       1.15.4  py27_blas_openblashb06ca3d_0  [blas_openblas]  conda-forge
-  #numpy-base  1.15.4  py27h2f8d375_0  
-  n_pts=on_voxels[0].shape[0]
-  coords=()
-  for i in range(num_dims):
-    coords+=(on_voxels[i].reshape(n_pts,1).astype('int64'),)
-  return np.concatenate(coords,axis=1).astype('int64') # locations have to be integer (indices)
-  # TODO: Test!     shape=(3817528,3)
-#============  end func def of  on_locs_nonzero(a):  ============
+def on_locs(a):
+  #================================================================
+  def on_locs_nonzero(a):
+    num_dims=3
+    on_voxels=np.nonzero(a) # as of Dec. 21, 2018 with the following versions, dtype is default 'int64'
+    #  numpy info from conda:
+    #python      2.7.15  h33da82c_4    conda-forge
+    #numpy       1.15.4  py27_blas_openblashb06ca3d_0  [blas_openblas]  conda-forge
+    #numpy-base  1.15.4  py27h2f8d375_0  
+    n_pts=on_voxels[0].shape[0]
+    coords=()
+    for i in range(num_dims):
+      coords+=(on_voxels[i].reshape(n_pts,1).astype('int64'),)
+    return np.concatenate(coords,axis=1).astype('int64') # locations have to be integer (indices)
+    # TODO: Test!     shape=(3817528,3)
+  #============  end func def of  on_locs_nonzero(a):  ============
+  return on_locs_nonzero(a)
+#================  end func def of  on_locs(a):  ================
 def rot8(model,angle,axis='z'):
   '''
     rot8s the model (counterclockwise from the top?  TODO: double-check this) angle, by default around the z axis
@@ -31,11 +35,12 @@ def rot8(model,angle,axis='z'):
     angle: in degrees, a float
   '''
   # TODO:   consider whether default behavior should be enlargen 3-D array, or "rotate into larger array" before cutting off the edges
-    # TODO: sub-concern is how to make sure u shift the "center" back to the right place after rot8ing the voxels
+
+# TODO: sub-concern is how to make sure u shift the "center" back to the right place after rot8ing the voxels
   assert model.shape[0] == model.shape[1] == model.shape[2] # model is cube-shaped
   print "angle (in degrees) is {0}".format(angle)
   angle = math.radians(angle)       # for use in cos(angle), sin(angle)
-  ons   = on_locs_nonzero(model).T  # Transposed so we can rotate the coords with R_z*coords
+  ons   = on_locs(model).T  # Transposed so we can rotate the coords with R_z*coords
   print "ons.shape is {0}".format(str(ons.shape))  # oughta have shape (big_num, 3)
   #=====================================================
   def shift(locs, delta):
@@ -160,24 +165,6 @@ def rot8(model,angle,axis='z'):
   rot8d[ons[:,0], ons[:,1], ons[:,2]] = True  # the voxels with these x,y,z values
   return rot8d
 #========= end func def of rot8(model,angle,axis='z'): =========
-
-'''
->>> y = np.arange(35).reshape(5,7)
->>> y
-array([[ 0,  1,  2,  3,  4,  5,  6],
-       [ 7,  8,  9, 10, 11, 12, 13],
-       [14, 15, 16, 17, 18, 19, 20],
-       [21, 22, 23, 24, 25, 26, 27],
-       [28, 29, 30, 31, 32, 33, 34]])
->>> y[np.array([0,2,4]), np.array([0,1,2])]
-===========================================================
-
-y = np.arange(35).reshape(5,7)
-print y
-y[np.array([0,2,4]).reshape((1,3)), np.array([0,1,2]).reshape((1,3))]
-
-'''
-
 
 
 
@@ -323,34 +310,3 @@ if __name__=='__main__':
 
 
 
-#================================================================
-def on_locs_random_timing_test(a,b=1,c=1):
-  '''
-    finds where is "on" in the rand array  (mainly for speed tests to see whether the naive for loop is viable)
-  '''
-  voxels=np.round(np.random.random((   a,  b,  c))).astype('bool') # prob a faster way to do this but who cares
-    # I think the above line was really slow and took up a lotta space b/c it had to first create the giant random array and THEN converted it all into "rounded booleans" BEFORE it could free up the original randoms array
-  #                                 (400,400,400)
-  return on_locs(voxels)
-#================================================================
-def on_locs(nparr):
-  '''
-    finds where is "on" in the array
-  '''
-  # NOTE:  weird!  this was fast for when I put it in model.py.  Dec. 20, 2018
-  on_locs=[]
-  for i in range(nparr.shape[0]): 
-    for j in range(nparr.shape[1]): 
-      for k in range(nparr.shape[2]): 
-        if nparr[i,j,k]:
-          on_locs.append(np.array([i,j,k]).reshape(3,1))
-          # I can already see ways to optimize this method, at least row-wise, by returning a whole nparray   instead of doing it this way
-          #   We could make on-locs a nparray and "cut off" the zeros at the end
-  return on_locs
-#================================================================
-def on_locs_argsort(a):
-  flat=a.reshape(np.prod(a.shape,1,1))
-  on_locs=np.argsort(flat)
-  on_locs=on_locs.reshape(a.shape)
-  return on_locs
-#================================================================

seg.py
@@ -122,7 +122,6 @@ def run_visualization(url, model):
   except IOError:
     print('Cannot retrieve image. Please check url: ' + url)
     return
-# end def run_visualization(url):
 
   print('running deeplab on image %s...' % url)
   resized_im, seg_map = model.run(original_im)
@@ -132,7 +131,8 @@ def run_visualization(url, model):
   #scipy.misc.imsave("_segmented____binary_mask_.jpg", np.greater(seg_map, PROBABLE).astype('bool'))
   if debug:
     vis_segmentation(resized_im, seg_map)
-  return seg_map
+  return np.rot90(seg_map,k=3) # k=3 b/c it gives us the result we want   (I tested it experimentally.  Dec. 26, 2018)
+#===== end func def of  run_visualization(url): =====
 
 
 def segment(IMG_URL):
@@ -164,6 +164,7 @@ def segment(IMG_URL):
   MODEL = DeepLabModel(download_path)
 
   return run_visualization(IMG_URL, MODEL)
+#===== end func def of  segment(IMG_URL): =====
 
 if __name__=='__main__':
   if len(sys.argv) == 1: