01f107f6bbed935510d0aad0170f3208ef122831
neonb88 <nathanbendich@gmail.com>
2019-02-10 22:48:04 +0000

    src/web/upload_img_flask/main.py:   This is the basic skeleton of what Pierlorenzo needs me to make.  The details are being a bit problematic right now, but I have to go get something to eat.

--- src/web/upload_img_flask/main.py
+++ src/web/upload_img_flask/main.py
@@ -2,6 +2,7 @@ import os
 from app import app
 from flask import Flask, flash, request, redirect, render_template
 from werkzeug.utils import secure_filename
+import time
 
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 IMG_FNAME=''
@@ -12,6 +13,7 @@ def allowed_file(filename):
 @app.route('/')
 @app.route('/upload', methods=['GET', 'POST', 'PUT'])
 def upload_form():
+  # TODO: eventually, this upload will be done automatically, like with send_from_directory(), instead of with a GUI form
 	return render_template('upload.html')
 
 @app.route('/uploaded', methods=['GET', 'POST', 'PUT'])
@@ -29,7 +31,21 @@ def upload_file():
 			filename = secure_filename(file.filename)
 			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 			flash('File(s) successfully uploaded')
-			return redirect('/upload')
+			#return redirect('/upload')
+			return redirect('/send_mesh')
+
+
+@app.route('/send_mesh', methods=['POST', 'PUT'])
+def send_mesh():
+  time.sleep(20.0) # NOTE: this might not work within Flask. TODO TODO TODO
+  return 'hi'
+  """
+  mesh_path='/x/p/fresh____as_of_Dec_12_2018/vr_mall____fresh___Dec_12_2018/src/web/upload_img_flask/mesh.obj'
+  with open('~/who.txt') as fp:
+    username=fp.read() # deals with the issue of changing AWS/GCloud instances
+  with open('/home/'+username+mesh_path, 'r') as fp:
+    return fp.read()
+  """
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True,port=80)
