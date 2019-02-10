20fb6af108b01beea0b4458d22dfc4fe5e32e422
neonb88 <nathanbendich@gmail.com>
2019-02-10 12:55:48 -0500

    I THINK I finally figured out the bug;  flask is too dumb to know what the shortcut  means.  So I have to write out the entire filepath each time and change it between machines.  Perhaps having 2 maintained * masteres is a good way to resolve this issue; but we'd need to be able to push the rest of the code between the 2 branches seamlessly.   Probably I'll just have to do it manually every time.

--- src/web/upload_img_flask/app.py
+++ src/web/upload_img_flask/app.py
@@ -1,7 +1,12 @@
 
 from flask import Flask
 
-UPLOAD_FOLDER = '~/x/p/fresh____as_of_Dec_12_2018/vr_mall____fresh___Dec_12_2018/src/web/upload_img_flask/customer_imgs'  # changing this didn't fix the bug (Sun Feb 10 12:32:26 EST 2019)
+UPLOAD_FOLDER = '/home/n/x/p/fresh____as_of_Dec_12_2018/vr_mall____fresh___Dec_12_2018/src/web/upload_img_flask/customer_imgs'  # changing this didn't fix the bug (Sun Feb 10 12:32:26 EST 2019)
+    # NOTE TODO
+    # whenever `git pull`ing, change port 5000 to port 80
+    #   and "/home/n/" to "/home/ubuntu/"
+    # and vice versa.
+
 
 app = Flask(__name__)
 app.secret_key = "secret key"
