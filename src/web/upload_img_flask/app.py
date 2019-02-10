381a535208173c669521a5ef82de48e69b55d09f
neonb88 <nathanbendich@gmail.com>
2019-02-10 17:39:47 +0000

    trying something for the web upload.  NOTE: this cod eis NOT working atm

--- src/web/upload_img_flask/app.py
+++ src/web/upload_img_flask/app.py
@@ -1,8 +1,7 @@
 
 from flask import Flask
 
-UPLOAD_FOLDER = '/home/n/x/p/fresh____as_of_Dec_12_2018/vr_mall____fresh___Dec_12_2018/src/web/upload_img_flask/customer_imgs'
-
+UPLOAD_FOLDER = '~/x/p/fresh____as_of_Dec_12_2018/vr_mall____fresh___Dec_12_2018/src/web/upload_img_flask/customer_imgs'  # changing this didn't fix the bug (Sun Feb 10 12:32:26 EST 2019)
 
 app = Flask(__name__)
 app.secret_key = "secret key"
--- src/web/upload_img_flask/main.py
+++ src/web/upload_img_flask/main.py
@@ -9,17 +9,12 @@ IMG_FNAME=''
 def allowed_file(filename):
 	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-def make_mesh(img_fname):
-  hmr_demo_loc='/home/n/Documents/code/old/hmr/'
-  os.system("conda activate cat && python2 -m {0}demo.py --img_path {1}".format(hmr_demo_loc,img_fname))
-  with open(hmr_demo_loc+"mesh.obj", 'r') as fp:
-    return fp.read()  # TODO: figure out how obj file will be communicated; sftp?  Pierlorenzo will know
-
 @app.route('/')
+@app.route('/upload', methods=['GET', 'POST', 'PUT'])
 def upload_form():
 	return render_template('upload.html')
 
-@app.route('/', methods=['POST'])
+@app.route('/uploaded', methods=['GET', 'POST', 'PUT'])
 def upload_file():
 	if request.method == 'POST':
         # check if the post request has the file part
@@ -33,13 +28,9 @@ def upload_file():
 		if file and allowed_file(file.filename):
 			filename = secure_filename(file.filename)
 			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-      #print(app.config['UPLOAD_FOLDER'])
-      #print(filename)
-      #print(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-      #IMG_FNAME= os.path.join(app.config['UPLOAD_FOLDER'], filename)
 			flash('File(s) successfully uploaded')
-			return redirect('/')
+			return redirect('/upload')
 
 if __name__ == "__main__":
-    app.run()
-    make_mesh(IMG_FNAME)
+    app.run(host='0.0.0.0',debug=True,port=80)
+
