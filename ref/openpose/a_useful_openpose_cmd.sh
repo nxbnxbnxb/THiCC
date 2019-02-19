b5149b522a4bab816490229f563fe900d8c56f7a
neonb88 <nathanbendich@gmail.com>
2019-02-18 19:08:59 -0500

    a_useful_openpose_cmd.sh:  this one oughta be minimal enough and just produce json (no annotated image)

--- ref/openpose/a_useful_openpose_cmd.sh
+++ ref/openpose/a_useful_openpose_cmd.sh
@@ -2,6 +2,10 @@
 
 # This one WORKS.  Before you throw in a million flags, make sure it WORKS (incremental improvements)
 cd ~/home/n/openpose &&\
-./build/examples/openpose/openpose.bin --image_dir examples/media/ --render_pose 0 --write_images output/ --display 0 --write_json output/ --write_json output/
+./build/examples/openpose/openpose.bin \
+  --image_dir /home/n/Pictures/small_openpose_1/ \
+  --render_pose 0 \
+  --display 0 \
+  --write_json output/
 # time to run :   Total time: 1501.079810 seconds.
 
