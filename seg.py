80e393d59655d07ba0557bb4fb2ec282f3d584f6
neonb88 <nathanbendich@gmail.com>
2019-02-20 22:51:49 -0500

    seg.py:   started overlay_img(imgfname1, imgfname2) function.  It is currently not working (ie. not complete)

--- seg.py
+++ seg.py
@@ -1,8 +1,13 @@
+WHITE=255
+BLACK=0
+TRANSPARENT=0
+
 import os, tarfile, tempfile
 import numpy as np
 np.seterr(all='raise')
 import tensorflow as tf
 import scipy
+from scipy.ndimage.measurements import center_of_mass as CoM
 from io import BytesIO
 import imageio as ii
 import skimage
@@ -219,6 +224,7 @@ def segment_local(local_filename):
   return seg_map(img, MODEL)
 #=====  end func def of   segment_local(local_filename) =====
 
+#==================================================
 def segment_URL(IMG_URL):
   '''
     NOTE: segmentation requires internet connection
@@ -251,9 +257,136 @@ def segment_URL(IMG_URL):
   return np.greater(run_visualization(IMG_URL, MODEL), FAIRLY_CERTAIN)
 #===== end func def of  segment_URL(IMG_URL): =====
 
+#====================================================================
+def overlay_imgs(img_fname_1, img_fname_2):
+    cutout1,mask1=segment_black_background(img_fname_1)
+    cutout2,mask2=segment_black_background(img_fname_2)
+    CoM1=CoM(mask1)
+    CoM2=CoM(mask2)
+
+    print("img_fname_1:\n",img_fname_1)
+    pltshow(cutout1)
+    print("img_fname_2:\n",img_fname_2)
+    pltshow(cutout2)
+    print("CoM1:\n",CoM1)
+    print("CoM2:\n",CoM2)
+    # TODO: finish this overlay_imgs() function!
+    pass 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#===== end func def of  overlay_imgs(img_fname_1, img_fname_2): =====
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#====================================================================
+def segment_transparent_background(local_fname):
+  # TODO: clear out messy comments, old print statements, etc.
+  # TODO:  put some of this code in a separate function
+  # TODO: cleanup like "segment_black_background(local_fname); all NOTES at the top, etc.
+  segmap= segment_local(local_fname)
+  img   = Image.open(local_fname)
+  # TODO: I really OUGHT to scale the mask to fit the dimensions of the image (we'd have better resolution this way)
+  segmap= segmap.reshape(segmap.shape[0],segmap.shape[1],1)
+  segmap=np.concatenate((segmap,segmap,segmap),axis=2)
+  segmap=np.rot90(segmap)
+
+  # NOTE:  PIL.resize(shape)'s shape has the width and height reversed from numpy's
+  img=img.resize((segmap.shape[1],segmap.shape[0]), Image.ANTIALIAS)
+
+  # weird stuff happened when I tried to convert to 'float64' in this `np.array(Image.open(fname))` line.
+  img = np.array(img) 
+
+  pltshow(img)
+  pltshow(segmap)
+  # as of (Wed Feb 20 17:49:37 EST 2019), segmap is 0 for background, 15 for human ( before astype('bool'))
+  segmap=segmap.astype('bool')
+  segmap=np.logical_not(segmap)
+  img[segmap]=WHITE
+  # cut out the human from the img
+  pltshow(img)
+  fname='person_cutout.png'
+  ii.imwrite(fname,img)
+  cutout=Image.open(fname)
+  os.system('rm '+fname) # cleaning up the intermediate step
+  cutout=cutout.convert('RGBA')
+  datas =cutout.getdata()
+  newData=[]
+  for item in datas:
+      if item[0] == WHITE and item[1] == WHITE and item[2] == WHITE:
+          newData.append((WHITE, WHITE, WHITE, TRANSPARENT))
+      else:
+          newData.append(item)
+  cutout.putdata(newData)
+  cutout.save("person_cutout_transparent_background.png", "PNG")
+  return img, np.logical_not(segmap) # NOTE: returns white background b/c can't return transparent in numpy arrs
+#======== end func def of  segment_transparent_background(params): ========
+
+#====================================================================
+def segment_black_background(local_fname):
+  '''
+    BLACK is 0; so this function can be used to "add" two images together to superimpose them.
+  '''
+  # NOTE:  PIL.resize(shape)'s shape has the width and height in the opposite order from numpy's height and width
+  # NOTE: weird sh!t happened when I tried to convert to 'float64' in the `np.array(Image.open(fname))` line.
+  segmap  = segment_local(local_fname)
+  segmap  = segmap.reshape(segmap.shape[0],segmap.shape[1],1)
+  segmap  = np.rot90(       np.concatenate((segmap,segmap,segmap),axis=2)        )
+  # I really OUGHT to scale the mask to fit the dimensions of the image (we'd have better resolution this way)
+  img     = Image.open(local_fname)
+  img     = img.resize((segmap.shape[1],segmap.shape[0]), Image.ANTIALIAS)
+  img     = np.array(img) 
+  if debug:
+    pltshow(img); pltshow(segmap)
+  # as of (Wed Feb 20 17:49:37 EST 2019), segmap is 0 for background, 15 for human ( before astype('bool'))
+  segmap=np.logical_not(segmap.astype('bool'))
+
+  # cut out the human from the img
+  img[segmap]=BLACK
+  pltshow(img)
+  fname='person_cutout__black_background.png'
+  ii.imwrite(fname,img)
+  return img, np.logical_not(segmap) # mask with 1s where there's a person, not where there's background
+#========== end func def of  segment_black_background(params): ==========
 
 if __name__=='__main__':
-  # TODO:  put this code in a separate function
   if len(sys.argv) == 1:
     IMG_URL = 'http://columbia.edu/~nxb2101/180.0.png'
     #'http://vishalanand.net/green.jpg'
@@ -262,41 +395,106 @@ if __name__=='__main__':
   else:
     img_path=sys.argv[1]
     #IMG_URL = sys.argv[1]# TODO: uncomment to segment images on the internet.
-  #segmap = segment_URL(IMG_URL) # TODO: uncomment to segment images on the internet.
-  segmap= segment_local(img_path)
-  print('segmap.shape: {0}'.format(segmap.shape))
   print('\n'*2)
-  img=Image.open(img_path)
-  #img=np_img(img_path) # I really OUGHT to scale the mask to fit the dimensions of the image (we'd have better resolution this way)
-  #print("img.shape : \n",img.shape)
-  segmap= segmap.reshape(segmap.shape[0],segmap.shape[1],1)
-  segmap=np.concatenate((segmap,segmap,segmap),axis=2)
-  segmap=np.rot90(segmap)
-  # 1920, 1080
-  #  513,  288
-  # NOTE: for NOW, I've hardcoded this;     TODO: change as soon as we're looking at a diff img.
-  #targ_shape=(640,360,3)
-  #pltshow(img.resize((segmap.shape[0],segmap.shape[1]), Image.ANTIALIAS))
-  img=img.resize((segmap.shape[1],segmap.shape[0]), Image.ANTIALIAS) #targ_shape, Image.ANTIALIAS)
-  print("type(img):\n",type(img))
-  # NOTE:  PIL.resize(shape)'s shape has the width and height reversed from numpy's
-  #segmap = utils.pad_color(segmap, targ_shape)
-  #img = np.asarray(img).astype('float64') np.asarray doesn't work on  type <class 'PIL.Image.Image'>
-  img = np.array(img)
-  pltshow(img)
-  print("img.dtype:\n",img.dtype)
-  print("   img.shape:\n", img.shape)
-  print("segmap.shape:\n", segmap.shape)
-  # as of (Wed Feb 20 17:49:37 EST 2019), segmap is 0 for background, 15 for human ( before astype('bool'))
-  pltshow(segmap)
-  segmap=segmap.astype('bool')
-  segmap=np.logical_not(segmap)
-  img[segmap]=0
-  # cut out the human from the img
-  pltshow(img)
-  ii.imwrite('nathan_cutout.png',img)
-  # how 2 make background transparent?
- 
+
+  fnames=sys.argv[1:] # TODO: change the analogous code in render_smpl.py
+  # TODO: if we extend seg.py (processing more cmd line args and more configuration parameters) the line `fnames=[sys.argv[1:]]` will break.
+  #   A better way to do it is with `import absl; absl.configs`
+  #     (see akanazawa's HMR for example of these configs.)
+
+  # TODO: error checking:
+  #   if len(sys.argv) < 3:   # should it be less than 3?  more?
+  overlay_imgs(fnames[0], fnames[1])  # tODO: hardcode the fnames?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # end if __name__=='__main__':
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  '''
+  #segmap = segment_URL(IMG_URL) # TODO: uncomment to segment images on the internet.
+  segment_transparent_background(img_path)
+  print('now doing black')
+  segment_black_background(img_path)
+  # TODO: more thorough tests.  For now, I'm going to move on b/c I wanna sleep and there are more important things.  But if you think of any bugs, please fix ASAP rather than 3 months from now.
+  '''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
