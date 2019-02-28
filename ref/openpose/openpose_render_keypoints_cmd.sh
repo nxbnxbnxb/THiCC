c3a7f4301a7394c1360fb59c34f7cce51190b026
neonb88 <nathanbendich@gmail.com>
2019-02-28 10:18:50 -0500

    actual openpose commands.  short and sweet and to-the-point

---
+++ ref/openpose/openpose_render_keypoints_cmd.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd /home/n/openpose && \
+./build/examples/openpose/openpose.bin \
+  --image_dir /home/n/Dropbox/vr_mall_backup/imgs/n8_front___jesus_legs_closed/ \
+  --write_images /home/n/Dropbox/vr_mall_backup/imgs/openpose_keypoints___n8_front_nude_jesus_pose_legs_closed___grassy_background_Newark_DE \
+  --render_pose  1 \
+  --display 0
+
+# OpenPose demo successfully finished. Total time: 269.407237 seconds.
+
